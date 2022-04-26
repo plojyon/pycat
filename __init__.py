7157846fe4506099ce6a502a875c206adcd0bbf3
Yon Ploj <yon.ploj@gmail.com>
2022-04-26 02:22:22 +0200

    Add content fill

--- __init__.py
+++ __init__.py
@@ -11,48 +11,64 @@ def get_terminal_size():
 
 
 class Window:
-    def __init__(self, location=(0, 0), size=(10, 10), style="double", title=""):
+    def __init__(
+        self, position=(0, 0), size=(10, 10), style="double", title="", fill=True
+    ):
         """Initialize an empty window.
 
-        :param location: A tuple of x,y coordinates for the top left outer corner
+        :param position: A tuple of x,y coordinates for the top left outer corner
         :param size: The outer size of a window (i.e. including borders)
         :param style: Window border style (see borders.py)
         :param title: Text displayed on the top of the window
+        :param fill: Whether the background of the window should be cleared
         """
         self.style = borders.STYLES[style]
         self.title = ""
-        self.location = location
+        self.position = position
         self.size = size
+        self.fill = fill
 
-    def draw(self, canvas):
-        """Draw own borders onto a given canvas."""
+    def _translate(self, x, y):
+        return (x + self.position[0], y + self.position[1])
+
+    def draw_border(self, canvas):
+        """Draw own border onto a given canvas."""
         # top, bottom
         for i in range(self.size[0] + 1):
-            border = 0
+            pos_up = self._translate(i, 0)
+            pos_down = self._translate(i, self.size[1])
             if i != 0:
-                border += borders.SIDES["left"] * self.style
+                canvas.set_border(pos_up, "left", self.style)
+                canvas.set_border(pos_down, "left", self.style)
             if i != self.size[0]:
-                border += borders.SIDES["right"] * self.style
-            # top
-            canvas.add_border((self.location[0] + i, self.location[1]), border)
-            # bottom
-            canvas.add_border(
-                (self.location[0] + i, self.location[1] + self.size[1]), border
-            )
+                canvas.set_border(pos_up, "right", self.style)
+                canvas.set_border(pos_down, "right", self.style)
+            # clear inner borders
+            if self.fill and i != 0 and i != self.size[0]:
+                canvas.set_border(pos_up, "down", 0)
+                canvas.set_border(pos_down, "up", 0)
 
         # left, right
         for j in range(self.size[1] + 1):
-            border = 0
+            pos_left = self._translate(0, j)
+            pos_right = self._translate(self.size[0], j)
             if j != 0:
-                border += borders.SIDES["up"] * self.style
+                canvas.set_border(pos_left, "up", self.style)
+                canvas.set_border(pos_right, "up", self.style)
             if j != self.size[1]:
-                border += borders.SIDES["down"] * self.style
-            # left
-            canvas.add_border((self.location[0], self.location[1] + j), border)
-            # right
-            canvas.add_border(
-                (self.location[0] + self.size[0], self.location[1] + j), border
-            )
+                canvas.set_border(pos_left, "down", self.style)
+                canvas.set_border(pos_right, "down", self.style)
+            # clear inner borders
+            if self.fill and j != 0 and j != self.size[1]:
+                canvas.set_border(pos_right, "left", 0)
+                canvas.set_border(pos_left, "right", 0)
+
+    def draw_content(self, canvas):
+        if self.fill:
+            for j in range(1, self.size[1]):
+                for i in range(1, self.size[0]):
+                    pos = self._translate(i, j)
+                    canvas.set_content(pos, self.fill_ch)
 
 
 class Canvas:
@@ -64,9 +80,9 @@ class Canvas:
         self.size = get_terminal_size()
         self.data = [[0 for j in range(self.size[1])] for i in range(self.size[0])]
 
-        self.windows = set(windows)
+        self.windows = windows
         if self.windows is None:
-            self.windows = set()
+            self.windows = []
 
     def add_border(self, position, border):
         """Overlay a border onto the character at a given position.
@@ -76,6 +92,9 @@ class Canvas:
         """
         if position[0] >= self.size[0] or position[1] >= self.size[1]:
             return
+
+        if isinstance(self.data[position[0]][position[1]], str):
+            self.data[position[0]][position[1]] = 0
         self.data[position[0]][position[1]] |= border
 
     def remove_border(self, position, border):
@@ -87,26 +106,54 @@ class Canvas:
         if position[0] >= self.size[0] or position[1] >= self.size[1]:
             return
 
-        mask = ~border & borders.mask_1(borders._bits_per_style * 4)
+        mask = ~border & borders.mask_1(borders.bits_per_style * 4)
+
+        if isinstance(self.data[position[0]][position[1]], str):
+            self.data[position[0]][position[1]] = 0
         self.data[position[0]][position[1]] &= mask
+
+    def set_border(self, position, side, style):
+        """Remove the existing border on a side and replace it with a given style.
+
+        :param position: A coordinte tuple for the new border
+        :param side: String "up"|"down"|"left"|"right"
+        :param style: An integer representing the new border type
+        """
+        self.remove_border(position, borders.mask_side(side))
+        self.add_border(position, borders.SIDES[side] * style)
+
+    def set_content(self, position, content):
+        if position[0] >= self.size[0] or position[1] >= self.size[1]:
+            return
+        self.data[position[0]][position[1]] = content
 
     def add_window(self, window):
         """Add a Window object to the canvas."""
-        window.canvas = self
-        self.windows += window
+        if window not in self.windows:
+            self.windows += window
+
+    def remove_window(self, window):
+        """Remove a Window object from canvas."""
+        if window in self.windows:
+            self.windows.remove(window)
 
     def redraw(self):
         """Scrap all canvas data and redraw all windows."""
         self.size = get_terminal_size()
         self.data = [[0 for j in range(self.size[1])] for i in range(self.size[0])]
         for window in self.windows:
-            window.draw(self)
+            window.draw_border(self)
+            window.draw_content(self)
 
     def print(self, debug=False):
         """Print the current canvas onto the terminal as-is, even if terminal resized."""
         for j in range(self.size[1]):
             for i in range(self.size[0]):
-                print(borders.get(self.data[i][j], debug), end="")
+                px = self.data[i][j]
+                if isinstance(px, int):  # px is a border type
+                    print(borders.get(px, debug), end="")
+                else:  # px is a content character
+                    print(px, end="")
             # last newline
             if j != self.size[1] - 1:
                 print()
@@ -116,7 +163,7 @@ class Canvas:
 
 def move(x, y):
     """Move the terminal cursor to x,y."""
-    print("\033[%d;%dH" % (y + 1, x + 1))
+    print("\033[%d;%dH" % (y + 1, x + 1), end="")
 
 
 import random
@@ -134,10 +181,18 @@ if __name__ == "__main__":
             * random.randint(0, 1)
             * random.randint(0, 1)
         )
-        win1 = Window(style="double", size=(10, 10), location=(1, 1))
-        win2 = Window(title="hello", size=(1 + i, 4), location=(6 + j, 9 + j))
-        win3 = Window(size=[i - 1 for i in get_terminal_size()], location=(0, 0))
-        canvas = Canvas([win1, win2, win3])
+        background = Window(size=[i - 1 for i in get_terminal_size()], style="double")
+        static = Window(style="thin", size=(10, 10), position=(2 + j, 1))
+        cat = Window(
+            style="thin", title="hello", size=(1 + i, 4), position=(6 + j, 9 + j)
+        )
+
+        background.fill_ch = "x"
+        static.fill_ch = "s"
+        cat.fill_ch = "c"
+
+        canvas = Canvas([background, static, cat])
         canvas.redraw()
         canvas.print(debug=False)
+        move(0, 0)
         time.sleep(0.1)
--- borders.py
+++ borders.py
@@ -7,21 +7,21 @@ Access a specific border type:
 from collections import defaultdict
 
 # This value should not exceed 32/4 = 8. Bad things might happen.
-_bits_per_style = 3  # allows up to 2 ** _bits_per_style different styles
+bits_per_style = 3  # allows up to 2 ** bits_per_style different styles
 
 STYLES = {
     "empty": 0b000,
     "double": 0b001,
     "thin": 0b010,
     "thick": 0b011,
-    # "full": 0b100,
+    "full": 0b100,
     ...: ...,  # add your own styles here
 }
 SIDES = {
-    "up": 1 << 3 * _bits_per_style,
-    "right": 1 << 2 * _bits_per_style,
-    "down": 1 << 1 * _bits_per_style,
-    "left": 1 << 0 * _bits_per_style,
+    "up": 1 << 3 * bits_per_style,
+    "right": 1 << 2 * bits_per_style,
+    "down": 1 << 1 * bits_per_style,
+    "left": 1 << 0 * bits_per_style,
 }
 
 
@@ -34,13 +34,18 @@ def mask_1(length):
     return mask
 
 
+def mask_side(side):
+    """Return a mask of ones at the given side."""
+    return SIDES[side] * mask_1(bits_per_style)
+
+
 def to_str(x):
-    mask = mask_1(_bits_per_style)
+    mask = mask_1(bits_per_style)
     sides = {
-        "up": (x & (mask * SIDES["up"])) >> 3 * _bits_per_style,
-        "right": (x & (mask * SIDES["right"])) >> 2 * _bits_per_style,
-        "down": (x & (mask * SIDES["down"])) >> 1 * _bits_per_style,
-        "left": (x & (mask * SIDES["left"])) >> 0 * _bits_per_style,
+        "up": (x & (mask * SIDES["up"])) >> 3 * bits_per_style,
+        "right": (x & (mask * SIDES["right"])) >> 2 * bits_per_style,
+        "down": (x & (mask * SIDES["down"])) >> 1 * bits_per_style,
+        "left": (x & (mask * SIDES["left"])) >> 0 * bits_per_style,
     }
 
     def int_to_style(key):
@@ -65,7 +70,7 @@ r = SIDES["right"]
 D = STYLES["double"]
 t = STYLES["thin"]
 T = STYLES["thick"]
-# F = STYLES["full"]
+F = STYLES["full"]
 
 BORDERS = {
     0: " ",
@@ -109,17 +114,17 @@ BORDERS = {
     l * t + r * t + d * t: "┬",
     u * t + l * t + r * t: "┴",
     u * t + l * t + r * t + d * t: "┼",
-    # l * F + r * F: "█",
-    # u * F + d * F: "█",
-    # r * F + d * F: "█",
-    # l * F + d * F: "█",
-    # u * F + r * F: "█",
-    # u * F + l * F: "█",
-    # u * F + r * F + d * F: "█",
-    # u * F + d * F + l * F: "█",
-    # l * F + r * F + d * F: "█",
-    # u * F + l * F + r * F: "█",
-    # u * F + l * F + r * F + d * F: "█",
+    l * F + r * F: "█",
+    u * F + d * F: "█",
+    r * F + d * F: "█",
+    l * F + d * F: "█",
+    u * F + r * F: "█",
+    u * F + l * F: "█",
+    u * F + r * F + d * F: "█",
+    u * F + d * F + l * F: "█",
+    l * F + r * F + d * F: "█",
+    u * F + l * F + r * F: "█",
+    u * F + l * F + r * F + d * F: "█",
     # add your own styles here
 }
 
